403b024c0737805e27774aaeb7b20eed0a2884e5
Lucas Su <lijie.su93@gmail.com>
2024-03-21 20:55:10 +1030

    update: repo name

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/today-i-learned/",
+  base: "/ai-lexicon-community/",
   // server: {
-  //   port: 5173,
+  //   watch:{
+  //     usePolling:true,
+  //   }
   // },
 });
